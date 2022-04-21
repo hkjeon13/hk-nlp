d2fa3265f09776790a6e415bc3b29ac2d2c90475
eddiejeon <eddie14@wisenut.co.kr>
2022-04-21 11:59:39 +0900

    Changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hk-nlp',  # 패키지 명
-      version='1.0.1.0',
+      version='1.0.1.1',
       description='personal package',
       author='psyche',
       author_email='hkjeon13@gmail.com',
