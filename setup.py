fc47b82824d9299be1538299972f507aaaa003c4
eddiejeon <eddie14@wisenut.co.kr>
2022-07-30 13:39:22 +0900

    Fix Absoulte Path For Synonym Replacement

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hk-nlp',  # 패키지 명
-      version='1.0.2.0',
+      version='1.0.2.1',
       description='personal package',
       author='psyche',
       author_email='hkjeon13@gmail.com',
